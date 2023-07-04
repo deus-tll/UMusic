65508115d2eb9cfe12ebb1a6cde946fce1a9d557
deus-tll <millerartemka14@gmail.com>
2023-07-04 23:48:48 +0300

    the construction of the window frame, and the creation of the top panel

--- UMusic_Application/Listener_ClientApp/MainWindow.xaml.cs
+++ UMusic_Application/Listener_ClientApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,79 @@ namespace Listener_ClientApp
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Point mousePos = e.GetPosition(this);
+
+			if (e.ChangedButton == MouseButton.Left &&
+				(mousePos.X >= 0 && mousePos.X < Grid_TopPanel.ActualWidth &&
+				mousePos.Y >= 0 && mousePos.Y < Grid_TopPanel.ActualHeight))
+				this.DragMove();
+		}
+
+
+		private void GoForwardBackward_Click(object sender, RoutedEventArgs e)
+		{
+			if (sender is not Button button) return;
+
+			string? tag = button.Tag as string;
+			if (string.IsNullOrEmpty(tag)) return;
+
+			switch (tag)
+			{
+				case "Forward":
+					if (MainFrame.CanGoForward)
+						MainFrame.GoForward();
+					break;
+
+				case "Backward":
+					if (MainFrame.CanGoBack)
+						MainFrame.GoBack();
+					break;
+			}
+		}
+
+
+		private void WindowPanelButtons_Click(object sender, RoutedEventArgs e)
+		{
+			if (sender is not Button button) return;
+
+			string? tag = button.Tag as string;
+			if (string.IsNullOrEmpty(tag)) return;
+
+			switch (tag)
+			{
+				case "Minimize":
+					WindowState = WindowState.Minimized;
+					break;
+				case "Maximize":
+					var style = (Style)Application.Current.Resources["WindowPanelButtonIcon"];
+
+					Action<WindowState> DefineState = (state) =>
+					{
+						WindowState = state;
+						ImageAwesome ico = new()
+						{
+							Icon = FontAwesomeIcon.WindowRestore,
+							Style = style
+						};
+
+						Btn_WindowMaximize.Content = ico;
+					};
+
+					if (WindowState == WindowState.Normal)
+						DefineState(WindowState.Maximized);
+					else
+						DefineState(WindowState.Normal);
+
+					break;
+				case "Close":
+					this.Close();
+					break;
+			}
 		}
 	}
 }
